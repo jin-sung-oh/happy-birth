25e98e5557c6720fc3de986e47c7ae4880fb40fa
jin-sung-oh <wlsqoren@naver.com>
2025-12-03 20:48:40 +0900

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path/win32'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/happy-birth/',  // GitHub Pages 저장소 이름
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
